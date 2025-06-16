fb482a958fad271da071eb2941c09734fd53cecf
xiaozaiyi <1932794922@qq.com>
2025-06-16 16:29:13 +0800

    chore: 更新版本号至1.0.1

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   manifest: {
     name: "浸入式学语言助手",
     description: "基于\"可理解输入\"理论的英语学习工具，帮助你在日常浏览中自然学习英语",
-    version: "1.0.0",
+    version: "1.0.1",
     permissions: ["storage", "tabs", "notifications"],
     host_permissions: ["<all_urls>"]
   },
